a94bc754100e927c06ed2c7817da668d8b001f3d
erenakgunduz <coderenius@gmail.com>
2023-04-20 00:14:23 -0400

    Update app.R
    really basic scaffold to get started

--- app.R
+++ app.R
@@ -1,9 +1,31 @@
 library(shiny)
 
 
-ui <- fluidPage()
+ui <- fluidPage(
+    titlePanel("Analysis of Street Fighter V Attacks"),
+    tabsetPanel(
+        tabPanel("Information"),
+        tabPanel("Visualization"),
+        tabPanel("Prediction")
+    )
+)
 
-server <- function(input, output) {
+information <- function(input, output, session) {
+    # how do the distributions look?
+}
+
+visualization <- function(input, output, session) {
+    # are onBlock and damage+stun correlated?
+}
+
+prediction <- function(input, output, session) {
+    # which regression will win: simple linear or elastic net?
+}
+
+server <- function(input, output, session) {
+    callModule(information, "Information")
+    callModule(visualization, "Visualization")
+    callModule(prediction, "Prediction")
 }
 
 
