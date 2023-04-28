5a80816301870ac8017d57d8c2964d62a486c696
erenakgunduz <coderenius@gmail.com>
2023-04-28 17:22:15 -0400

    Update app.R
    Should be structured better now

--- app.R
+++ app.R
@@ -1,32 +1,85 @@
 source("global.R")
 
+df <- read.csv("data/all.csv", stringsAsFactors = FALSE)
+
 
 ui <- fluidPage(
-    titlePanel("Analysis of Street Fighter V Attacks"),
-    tabsetPanel(
+  titlePanel("Analysis of Street Fighter V Attacks"),
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput("sidebar_options")
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = "tab",
+        type = "tabs",
         tabPanel("Information"),
         tabPanel("Visualization"),
         tabPanel("Prediction")
+      ),
     )
+  )
 )
 
-information <- function(input, output, session) {
-    # how do the distributions look?
+information_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("character"), "Select character:",
+      choices = c("All", unique(df$Character))
+    ),
+    selectInput(ns("examine_var"), "Variable to examine",
+      choices = c("Frames on block", "Damage", "Stun")
+    )
+  )
 }
 
-visualization <- function(input, output, session) {
-    # are onBlock and damage+stun correlated?
+visualization_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("character"), "Select character:",
+      choices = c("All", unique(df$Character))
+    ),
+    selectInput(ns("vislize_var"), "Variable(s)",
+      choices = c("Both", "Damage", "Stun")
+    )
+  )
 }
 
-prediction <- function(input, output, session) {
-    # which regression will win: simple linear or elastic net?
+prediction_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("response_var"), "Want to predict:",
+      choices = c("Damage", "Stun")
+    )
+  )
 }
 
-server <- function(input, output, session) {
-    callModule(information, "Information")
-    callModule(visualization, "Visualization")
-    callModule(prediction, "Prediction")
+information_server <- function(input, output, session) {
+  # how do the distributions look?
+}
+
+visualization_server <- function(input, output, session) {
+  # are onBlock and damage+stun correlated?
+}
+
+prediction_server <- function(input, output, session) {
+  # which regression will win: simple linear or elastic net?
+}
+
+server <- function(input, output) {
+  output$sidebar_options <- renderUI({
+    tab <- switch(as.character(input$tab),
+      "Information" = information_ui("Information"),
+      "Visualization" = visualization_ui("Visualization"),
+      "Prediction" = prediction_ui("Prediction")
+    )
+    tab
+  })
+
+  callModule(information_server, "Information")
+  callModule(visualization_server, "Visualization")
+  callModule(prediction_server, "Prediction")
 }
 
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
